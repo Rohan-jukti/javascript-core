47f9ef881d69a215c4bde665dc63d990075da68a
Rohan Jukti <rohanjukti.in@gmail.com>
2026-04-06 13:01:36 +0530

    update repo

---
+++ Module 1/10.searching/searching.js
@@ -0,0 +1,18 @@
+//find the most occurance in the arr
+
+// function search (arr,target){
+//     let count =0
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i] === target){
+//         count++;
+//         }
+//     }if(count == 0){
+//         return -1
+//     }else{
+//         return count
+//     }
+// }
+// console.log(search([1,2,3,4,4,5],4))
+
+// ------------------------------------------------------------------------------
+
---
+++ Module 2/01.Nested/nested.js
@@ -0,0 +1,81 @@
+// let count = 0
+// for(let i=0; i<3; i++){
+//     for(let j=0; j<3; j++){
+//         count++;
+//     }
+// }
+// console.log(count);
+//if outer loop is  running n number of times and inner loop is running a number of times
+//then the whole nested loop runs n*m number of times
+
+//Q1. print the below pattern
+// #####
+// #####
+// #####
+// #####
+// for (let i=1;i<4;i++){
+//     let p=""
+//     for(let j=0;j<=4;j++){
+//         p+="#"
+//     }
+//     console.log(p)
+// }
+
+
+//Q2. print rectangle
+// ###########
+// ###########
+// ###########
+// let a = 4
+// let b = 10
+// for (let i=1;i<a;i++){
+//     let p=""
+//     for(let j=0;j<=b;j++){
+//         p+="#"
+//     }
+//     console.log(p)
+// }
+
+
+//Q3. print matrix table 
+// 00 01 02 03 
+// 10 11 12 13 
+// 20 21 22 23 
+// 30 31 32 33 
+// for (let i=0;i<4;i++){
+//     let p=""
+//     for(let j=0;j<4;j++){
+//         p+=`${i}${j} `
+//     }
+//     console.log(p)
+// }
+
+
+//Q4.print the below pattern
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+// for (let i = 5; i > 0; i--) {
+//     let p = "";
+//     for (let j = 0; j < i; j++) {
+//         p += "* ";
+//     }
+//     console.log(p);
+// }
+
+
+//Q5.print the below pattern
+// A 
+// A B 
+// A B C 
+// A B C D 
+// let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// for (let i = 0; i < 4; i++) {
+//     let p = "";
+//     for (let j = 0; j <= i; j++) {
+//         p += alphabet[j] + " ";
+//     }
+//     console.log(p);
+// }
